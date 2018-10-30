ce9b8163fc564d44d05e52bf294f7b70eced0b5a
Chris Howard <Chris.Howard@loopup.com>
2018-10-29 19:46:37 -0700

    Add input functionality
    Creates handler for determining what an input field should look like
    after it has been edited by a user. Also includes helper function for
    attaching the handler to input DOM elements.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'teleformat-input.js',
+    library: 'teleformatInput',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
